c43ed43ebe9e52130fbacf06f2182a0a4432230b
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-02-11 12:00:46 +0530

    shop and directory reducer created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger'; // for debugging redux code
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store)
